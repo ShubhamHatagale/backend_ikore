4c6f6d4c774326af74ea462e33f7425ad4eadeb5
shubham <shubhamhatagale02@gmail.com>
2022-12-15 18:43:12 +0530

    18:4215Dec2022

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Router=require('./routes/index');
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
+ 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
--- controllers/EnrollController.js
+++ controllers/EnrollController.js
@@ -4,6 +4,7 @@ const transporter = require("../config/mailConfig");
 module.exports = (req, resp) => {
     const { name, email, phone, course, message } = req.body;
     var success = 0;
+    const mailId=`admin@ikorepilates.com`
     if (name && email && phone && course && message) {
         success = connection.query("insert into user(Name,Email_id,Phone_no,message,course) values(?,?,?,?,?)", [name, email, phone, message, course], (error) => {
             if (error)
@@ -14,9 +15,19 @@ module.exports = (req, resp) => {
     if (success) {
         const emailConfig = {
             from: process.env.email_user,
-            to: email,
+            to: mailId,
             subject: 'feedback email',
-            text: "Thank you for contacting us. We will get back to you soon"
+            text: `Thank you for contacting us. We will get back to you soon`,
+            html:`<p>Dear ${name}</p>
+            <b>Thank you for contacting us. We will get back to you soon</b>
+            <p>name: ${name}</p>
+                <p>email: ${mailId}</p>
+                <p>phone: ${phone}</p>
+                <p>Course: ${course}</p>
+            <p>Please also be sure to visit our <a href="http://phpstack-895831-3108674.cloudwaysapps.com/">FAQs</a> </p>
+            <br/><p>Thank you for your patronage</p>
+            <p>ikore pilates Team</p>
+            <p> <a href="http://phpstack-895831-3108674.cloudwaysapps.com/">ikore pilates</a></p>`
         }
         transporter.sendMail(emailConfig, (error, info) => {
             if (error) {
