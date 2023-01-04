c0ce724ebd11ed50803c2e64813a239c110ed2cc
shubham <shubhamhatagale02@gmail.com>
2023-01-04 16:55:14 +0530

    backendR4Jan2022

--- controllers/EnrollController.js
+++ controllers/EnrollController.js
@@ -2,32 +2,33 @@ const connection = require("../config/configDB");
 const transporter = require("../config/mailConfig");
 
 module.exports = (req, resp) => {
+    
     const { name, email, phone, course, message } = req.body;
     var success = 0;
-    const mailId=`admin@ikorepilates.com`
+    const mailId=email
+    // `admin@ikorepilates.com`
     if (name && email && phone && course && message) {
         success = connection.query("insert into user(Name,Email_id,Phone_no,message,course) values(?,?,?,?,?)", [name, email, phone, message, course], (error) => {
             if (error)
                 throw error;
         });
-
+        
     }
     if (success) {
         const emailConfig = {
-            from: process.env.email_user,
-            to: mailId,
-            subject: 'feedback email',
-            text: `Thank you for contacting us. We will get back to you soon`,
-            html:`<p>Dear ${name}</p>
-            <b>Thank you for contacting us. We will get back to you soon</b>
+            from:'no-reply@ikorepilates.com',
+            to: ['admin@ikorepilates.com','nitish.conversant@gmail.com'],
+            subject: `Contact Details of ${name}`,
+            //text: `Thank you for contacting us. We will get back to you soon`,
+            html:`<p>Dear Admin</p>
+            
+            <b>please check below contact details</b>
             <p>name: ${name}</p>
                 <p>email: ${mailId}</p>
                 <p>phone: ${phone}</p>
-                <p>Course: ${course}</p>
-            <p>Please also be sure to visit our <a href="http://phpstack-895831-3108674.cloudwaysapps.com/">FAQs</a> </p>
-            <br/><p>Thank you for your patronage</p>
-            <p>ikore pilates Team</p>
-            <p> <a href="http://phpstack-895831-3108674.cloudwaysapps.com/">ikore pilates</a></p>`
+                <p>Course: ${course}</p>`
+        
+            
         }
         transporter.sendMail(emailConfig, (error, info) => {
             if (error) {
