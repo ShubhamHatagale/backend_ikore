9d8b16e47380da3cfd17496d445a01d9669882fc
shubham <shubhamhatagale02@gmail.com>
2023-03-28 12:24:07 +0530

    28Mar2023

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const route=express.Router();
 const Router=require('./routes/index');
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }))
+ app.use(bodyParser.urlencoded({ extended: true }))
  
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
